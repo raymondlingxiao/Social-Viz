f28e48788b0981a83a81f1b56335bad5e33e108e
raymondlingxiao <me@raymondlx.com>
2019-03-25 21:34:21 -0500

    update data

--- data_process/data_clean.py
+++ data_process/data_clean.py
@@ -36,13 +36,23 @@ for i in range(start_year, end_year+1, 1):
         for row in reader:
             month = row[0]
             state = row[1]
-            permit = row[2]
+            handgun = 0
+            long_gun = 0
+
+            if (len(row[4]) != 0):
+                handgun = int(row[4]) * 1.1
+
+            if (len(row[5]) != 0):
+                long_gun = int(row[5]) * 1.1
+            multi_gun = int(row[7]) * 2
+
+            permit = int(handgun + long_gun + multi_gun)
 
             # exclude Guam and other states which not exists in database
             if (str(i) in month and state in pair):
                 if (state not in permits):
                     permits[state] = {}
-                    permits[state]["permit"] = int(permit)
+                    permits[state]["permit"] = permit
                     permits[state]["id"] = pair[state]
                 else:
                     permits[state]["permit"] = permits[state]["permit"] + int(permit)
@@ -52,7 +62,7 @@ pprint(permits_list)
 
 # save as json file
 
-out_put_file = './firearm.json'
+out_put_file = './firearm_v2.json'
 with open(out_put_file,'w') as file:
     json.dump(permits_list,file)
 
