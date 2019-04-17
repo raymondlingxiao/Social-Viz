218667013758e51f7598775e1eda4e16bf4919a2
Xiaolu85 <xlqizju@gmail.com>
2019-04-17 00:13:41 -0500

    checkbox

--- script.js
+++ script.js
@@ -18,58 +18,58 @@ slider.oninput = function () {
 
 // State location data
 const stateData = {
-	"Alaska": [-154.493062, 63.588753],
-	"Alabama": [-86.902298, 32.318231],
-	"Arkansas": [-91.831833, 35.20105],
-	"Arizona": [-111.093731, 34.048928],
-	"California": [-119.417932, 36.778261],
-	"Colorado": [-105.782067, 39.550051],
-	"Connecticut": [-73.087749, 41.603221],
-	"District of Columbia": [-77.033418, 38.905985],
-	"Delaware": [-75.52767, 38.910832],
-	"Florida": [-81.515754, 27.664827],
-	"Georgia": [-82.907123, 32.157435],
-	"Hawaii": [-155.665857, 19.898682],
-	"Iowa": [-93.097702, 41.878003],
-	"Idaho": [-114.742041, 44.068202],
-	"Illinois": [-89.398528, 40.633125],
-	"Indiana": [-85.602364, 40.551217],
-	"Kansas": [-98.484246, 39.011902],
-	"Kentucky": [-84.270018, 37.839333],
-	"Louisiana": [-92.145024, 31.244823],
-	"Massachusetts": [-71.382437, 42.407211],
-	"Maryland": [-76.641271, 39.045755],
-	"Maine": [-69.445469, 45.253783],
-	"Michigan": [-85.602364, 44.314844],
-	"Minnesota": [-94.6859, 46.729553],
-	"Missouri": [-91.831833, 37.964253],
-	"Mississippi": [-89.398528, 32.354668],
-	"Montana": [-110.362566, 46.879682],
-	"North Carolina": [-79.0193, 35.759573],
-	"North Dakota": [-101.002012, 47.551493],
-	"Nebraska": [-99.901813, 41.492537],
-	"New Hampshire": [-71.572395, 43.193852],
-	"New Jersey": [-74.405661, 40.058324],
-	"New Mexico": [-105.032363, 34.97273],
-	"Nevada": [-116.419389, 38.80261],
-	"New York": [-74.217933, 43.299428],
-	"Ohio": [-82.907123, 40.417287],
-	"Oklahoma": [-97.092877, 35.007752],
-	"Oregon": [-120.554201, 43.804133],
-	"Pennsylvania": [-77.194525, 41.203322],
-	"Puerto Rico": [-66.590149, 18.220833],
-	"Rhode Island": [-71.477429, 41.580095],
-	"South Carolina": [-81.163725, 33.836081],
-	"South Dakota": [-99.901813, 43.969515],
-	"Tennessee": [-86.580447, 35.517491],
-	"Texas": [-99.901813, 31.968599],
-	"Utah": [-111.093731, 39.32098],
-	"Virginia": [-78.656894, 37.431573],
-	"Vermont": [-72.577841, 44.558803],
-	"Washington": [-120.740139, 47.751074],
-	"Wisconsin": [-88.787868, 43.78444],
-	"West Virginia": [-80.454903, 38.597626],
-	"Wyoming": [-107.290284, 43.075968]
+    "Alaska": [-154.493062, 63.588753],
+    "Alabama": [-86.902298, 32.318231],
+    "Arkansas": [-91.831833, 35.20105],
+    "Arizona": [-111.093731, 34.048928],
+    "California": [-119.417932, 36.778261],
+    "Colorado": [-105.782067, 39.550051],
+    "Connecticut": [-73.087749, 41.603221],
+    "District of Columbia": [-77.033418, 38.905985],
+    "Delaware": [-75.52767, 38.910832],
+    "Florida": [-81.515754, 27.664827],
+    "Georgia": [-82.907123, 32.157435],
+    "Hawaii": [-155.665857, 19.898682],
+    "Iowa": [-93.097702, 41.878003],
+    "Idaho": [-114.742041, 44.068202],
+    "Illinois": [-89.398528, 40.633125],
+    "Indiana": [-85.602364, 40.551217],
+    "Kansas": [-98.484246, 39.011902],
+    "Kentucky": [-84.270018, 37.839333],
+    "Louisiana": [-92.145024, 31.244823],
+    "Massachusetts": [-71.382437, 42.407211],
+    "Maryland": [-76.641271, 39.045755],
+    "Maine": [-69.445469, 45.253783],
+    "Michigan": [-85.602364, 44.314844],
+    "Minnesota": [-94.6859, 46.729553],
+    "Missouri": [-91.831833, 37.964253],
+    "Mississippi": [-89.398528, 32.354668],
+    "Montana": [-110.362566, 46.879682],
+    "North Carolina": [-79.0193, 35.759573],
+    "North Dakota": [-101.002012, 47.551493],
+    "Nebraska": [-99.901813, 41.492537],
+    "New Hampshire": [-71.572395, 43.193852],
+    "New Jersey": [-74.405661, 40.058324],
+    "New Mexico": [-105.032363, 34.97273],
+    "Nevada": [-116.419389, 38.80261],
+    "New York": [-74.217933, 43.299428],
+    "Ohio": [-82.907123, 40.417287],
+    "Oklahoma": [-97.092877, 35.007752],
+    "Oregon": [-120.554201, 43.804133],
+    "Pennsylvania": [-77.194525, 41.203322],
+    "Puerto Rico": [-66.590149, 18.220833],
+    "Rhode Island": [-71.477429, 41.580095],
+    "South Carolina": [-81.163725, 33.836081],
+    "South Dakota": [-99.901813, 43.969515],
+    "Tennessee": [-86.580447, 35.517491],
+    "Texas": [-99.901813, 31.968599],
+    "Utah": [-111.093731, 39.32098],
+    "Virginia": [-78.656894, 37.431573],
+    "Vermont": [-72.577841, 44.558803],
+    "Washington": [-120.740139, 47.751074],
+    "Wisconsin": [-88.787868, 43.78444],
+    "West Virginia": [-80.454903, 38.597626],
+    "Wyoming": [-107.290284, 43.075968]
 }
 
 // global map
@@ -95,22 +95,20 @@ function police_controller() {
         filter_button_3.style.backgroundColor = "white";
     }
 
-    if (map.getLayoutProperty('gunfire_points','visibility') === 'visible'){
-        map.setLayoutProperty('gunfire_points','visibility','none');
+    if (map.getLayoutProperty('gunfire_points', 'visibility') === 'visible') {
+        map.setLayoutProperty('gunfire_points', 'visibility', 'none');
         policeStatus = false;
-    }
-    else{
-        map.setLayoutProperty('gunfire_points','visibility','visible');
+    } else {
+        map.setLayoutProperty('gunfire_points', 'visibility', 'visible');
         policeStatus = true;
     }
 
-    if (map.getLayoutProperty('other_points','visibility') === 'visible'){
-        map.setLayoutProperty('other_points','visibility','none');
+    if (map.getLayoutProperty('other_points', 'visibility') === 'visible') {
+        map.setLayoutProperty('other_points', 'visibility', 'none');
         policeStatus = false;
 
-    }
-    else{
-        map.setLayoutProperty('other_points','visibility','visible');
+    } else {
+        map.setLayoutProperty('other_points', 'visibility', 'visible');
         policeStatus = true;
     }
 }
@@ -128,12 +126,11 @@ function mass_shooting_controller() {
     }
 
 
-    if (map.getLayoutProperty('mass_shooting','visibility') === 'visible'){
-        map.setLayoutProperty('mass_shooting','visibility','none');
+    if (map.getLayoutProperty('mass_shooting', 'visibility') === 'visible') {
+        map.setLayoutProperty('mass_shooting', 'visibility', 'none');
         massStatus = false;
-    }
-    else{
-        map.setLayoutProperty('mass_shooting','visibility','visible');
+    } else {
+        map.setLayoutProperty('mass_shooting', 'visibility', 'visible');
         massStatus = true;
     }
 }
@@ -157,8 +154,7 @@ function gun_sale_controller() {
         // map.setLayoutProperty('3dLayer', 'visibility', 'none');
         map.setLayoutProperty('stateLayer', 'visibility', 'none');
 
-    }
-    else {
+    } else {
         // map.setLayoutProperty('3dLayer', 'visibility', 'none');
         map.setLayoutProperty('stateLayer', 'visibility', 'visible');
     }
@@ -176,12 +172,11 @@ function hunting_controller() {
         filter_button_4.style.backgroundColor = "white";
     }
 
-    if (map.getLayoutProperty('hunting_bars','visibility') === 'visible'){
-        map.setLayoutProperty('hunting_bars','visibility','none');
+    if (map.getLayoutProperty('hunting_bars', 'visibility') === 'visible') {
+        map.setLayoutProperty('hunting_bars', 'visibility', 'none');
         huntStatus = false;
-    }
-    else{
-        map.setLayoutProperty('hunting_bars','visibility','visible');
+    } else {
+        map.setLayoutProperty('hunting_bars', 'visibility', 'visible');
         huntStatus = true;
     }
 
@@ -230,16 +225,16 @@ function choose(year) {
         return data;
     }
 
-// set legend range
+    // set legend range
 
     function getColor(number) {
         return number > 1000000 ? 'rgb(21, 20, 20)' :
             number > 600000 ? 'rgb(40, 38, 38)' :
-                number > 300000 ? 'rgb(58, 55, 55)' :
-                    number > 200000 ? 'rgb(84, 79, 79)' :
-                        number > 150000 ? 'rgb(97, 94, 94)' :
-                            number > 100000 ? 'rgb(116, 113, 113)' :
-                                'rgb(133, 130, 130)';
+            number > 300000 ? 'rgb(58, 55, 55)' :
+            number > 200000 ? 'rgb(84, 79, 79)' :
+            number > 150000 ? 'rgb(97, 94, 94)' :
+            number > 100000 ? 'rgb(116, 113, 113)' :
+            'rgb(133, 130, 130)';
     }
 
 
@@ -298,7 +293,7 @@ function choose(year) {
     })
 
 
-// create pop up
+    // create pop up
     map.on('click', function (e) {
         let features = map.queryRenderedFeatures(e.point);
         let clicked = null;
@@ -366,7 +361,10 @@ function choose(year) {
             if (parseInt(data_cur[key]['permit']) < min)
                 min = parseInt(data_cur[key]['permit']);
 
-            data_norm.push({"STATE_ID": data_cur[key]['id'], "PERMIT_NUM": parseInt(data_cur[key]['permit'])});
+            data_norm.push({
+                "STATE_ID": data_cur[key]['id'],
+                "PERMIT_NUM": parseInt(data_cur[key]['permit'])
+            });
         }
 
         console.log(data_norm);
@@ -384,7 +382,10 @@ function choose(year) {
                     const channels = 4;
                     let data = new Uint8Array(barWidth * height * channels);
 
-                    const r = 93, g = 189, b = 234, a = 200;
+                    const r = 93,
+                        g = 189,
+                        b = 234,
+                        a = 200;
 
                     for (let x = 0; x < barWidth; x++) {
                         for (let y = height - 1; y >= 0; y--) {
@@ -401,7 +402,8 @@ function choose(year) {
                 features = []
                 data = huntingData[year.toString()];
 
-                let min = 1, max = -1;
+                let min = 1,
+                    max = -1;
 
                 const getMinMaxVal = (rangeMin, rangeMax, curVal) => {
                     return (rangeMax - rangeMin) / (max - min) * (curVal - max) + rangeMax;
@@ -419,7 +421,11 @@ function choose(year) {
                     var loc = stateData[state];
                     const height = Math.ceil(getMinMaxVal(20, 90, val["huntingperperson"]));
                     const bar = generateBar(height);
-                    map.addImage(`bar_${state}`, {width: barWidth, height: height, data: bar});
+                    map.addImage(`bar_${state}`, {
+                        width: barWidth,
+                        height: height,
+                        data: bar
+                    });
                     features.push({
                         "type": "Feature",
                         "properties": {
@@ -478,7 +484,8 @@ function choose(year) {
 
                 map.on('mousemove', function (e) {
                     showDetail(e, 'stateLayer', ["State: ", "Population: ",
-                        "# Hunting Permit: ", "% Hunting Permit: "])
+                        "# Hunting Permit: ", "% Hunting Permit: "
+                    ])
                 });
 
             });
@@ -707,4 +714,3 @@ function choose(year) {
         });
     })
 }
-
